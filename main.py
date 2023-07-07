35b9b5b3d71eb27f581aca0d6f3060e5035498a5
zehraina <inazehra6@gmail.com>
2023-07-07 18:35:59 +0530

    Created a basic structure

---
+++ main.py
@@ -0,0 +1,37 @@
+class Player(self, name, bowling, batting, fielding, running, experience):
+    self.name=name
+    self.bowling=bowling
+    self.batting=batting
+    self.fielding=fielding
+    self.running=running
+    self.experience=experience
+    
+class Teams(self):
+    def select_caption():
+        pass
+    def next_player():
+        pass
+    def select_bowler():
+        pass
+    def batting_order():
+        pass
+    
+class Field(self, field_size, fan_ratio, pitch_conditions, home_advantage):
+    pass
+
+class Umpire(self):
+    def make_deicision():
+        pass
+    
+class Commentator(self):
+    pass
+
+class Match(self, Teams, Field, Umpire):
+    def start_match():
+        pass
+    
+    def change_innings():
+        pass
+    
+    def end_match():
+        pass
